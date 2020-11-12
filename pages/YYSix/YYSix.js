d0088f4538b13a54e8e66ef43b092d971b8feeed
nelson <nelson_l@sina.com>
2020-11-12 18:19:43 +0800

    根据新蓝牙协议修改

--- pages/YYSix/YYSix.js
+++ pages/YYSix/YYSix.js
@@ -1,296 +1,323 @@
 const AppData = getApp().globalData;
 const DeviceFunction = require('../../utils/BLE/deviceFuntion')
+import Dialog from 'vant-weapp/dialog/dialog';
+
 
 Page({
-	data: {
-		scrollArrTiming: {
-			'10': 310,
-			'20': 240,
-			'30': 178,
-			'40': 120,
-			'50': 60,
-			'60': 0
-		},
-		scrollArrDegree: {
-			'1': 138,
-			'2': 70,
-			'3': 0
-		},
-		kneadScrollNum: 0,
-		bodyHeight: 0,
-		temperature: 0,
-		timing: 0,
-		degree: 0,
-		power: false,
-		kneadModel: 0,
-		kneadDegree: 0,
-	},
+    data: {
+        scrollArrTiming: {
+            '10': 310,
+            '20': 240,
+            '30': 178,
+            '40': 120,
+            '50': 60,
+            '60': 0
+        },
+        scrollArrDegree: {
+            '1': 138,
+            '2': 70,
+            '3': 0
+        },
+        kneadScrollNum: 0,
+        bodyHeight: 0,
+        temperature: 0,
+        timing: 0,
+        degree: 0,
+        power: false,
+        kneadModel: 0,
+        kneadDegree: 0,
+        tempList: [10, 20, 30, 40, 50, 60],
+        tempPower: false,
+        tempTiminig: 30,
+        showDialog: false,
+    },
 
-	onLoad: function (options) {
-		let objD = Object.assign({}, this.data.scrollArrDegree),
-			objTi = Object.assign({}, this.data.scrollArrTiming)
-		for (let i in objD) {
-			objD[i] = objD[i] / AppData.widthProp;
-		}
-		for (let i in objTi) {
-			objTi[i] = objTi[i] / AppData.widthProp;
-		}
-		this.setData({
-			bodyHeight: AppData.windowHeight - AppData.menuButtonTop - AppData.menuBtnHeight - 12,
-			scrollArrTiming: objTi,
-			scrollArrDegree: objD,
-		})
-	},
+    onLoad: function (options) {
+        let objD = Object.assign({}, this.data.scrollArrDegree),
+            objTi = Object.assign({}, this.data.scrollArrTiming)
+        for (let i in objD) {
+            objD[i] = objD[i] / AppData.widthProp;
+        }
+        for (let i in objTi) {
+            objTi[i] = objTi[i] / AppData.widthProp;
+        }
+        this.setData({
+            bodyHeight: AppData.windowHeight - AppData.menuButtonTop - AppData.menuBtnHeight - 12,
+            scrollArrTiming: objTi,
+            scrollArrDegree: objD,
+        })
+    },
 
-	onShow: function () {
-		this.handleGetValue()
-	},
+    onShow: function () {
+        this.handleGetValue()
+    },
 
-	handleGetValue() {
-		this.setData({
-			temperature: AppData.temperature,
-			timing: AppData.timing,
-			kneadModel: AppData.kneadDirection,
-			kneadDegree: AppData.kneadDegree,
-			power: AppData.timing == 0 ? false : true
-		})
-	},
+    handleGetValue() {
+        this.setData({
+            temperature: 45,
+            timing: 50,
+            kneadModel: AppData.kneadDirection,
+            kneadDegree: AppData.kneadDegree,
+            power: true
+        })
+    },
 
-	// 拖拽控件
-	handleTouchEndTime: function (e) {
-		let num = e.changedTouches[0].clientX,
-			time = 0;
-		// 37 97 157 217 277 337
-		if (num > 307) {
-			time = 60
-		} else if (num <= 307 && num > 247) {
-			time = 50
-		} else if (num <= 247 && num > 187) {
-			time = 40
-		} else if (num <= 187 && num > 127) {
-			time = 30
-		} else if (num <= 127 && num > 67) {
-			time = 20
-		} else if (num <= 67) {
-			time = 10
-		}
-		this.handleSwitchTimer(time)
-	},
+    // 拖拽控件
+    handleTouchEndTime: function (e) {
+        let num = e.changedTouches[0].clientX,
+            time = 0;
+        // 37 97 157 217 277 337
+        if (num > 307) {
+            time = 60
+        } else if (num <= 307 && num > 247) {
+            time = 50
+        } else if (num <= 247 && num > 187) {
+            time = 40
+        } else if (num <= 187 && num > 127) {
+            time = 30
+        } else if (num <= 127 && num > 67) {
+            time = 20
+        } else if (num <= 67) {
+            time = 10
+        }
+        this.handleSwitchTimer(time)
+    },
 
-	handleTouchEndKnead: function (e) {
-		let num = e.changedTouches[0].clientX,
-			degree = 0;
-		// 206 276 346 
-		if (num <= 241) {
-			degree = 1
-		} else if (num > 241 && num <= 311) {
-			degree = 2
-		} else if (num > 311) {
-			degree = 3
-		}
-		this.handleKneadDegree(degree)
-	},
+    handleTouchEndKnead: function (e) {
+        let num = e.changedTouches[0].clientX,
+            degree = 0;
+        // 206 276 346 
+        if (num <= 241) {
+            degree = 1
+        } else if (num > 241 && num <= 311) {
+            degree = 2
+        } else if (num > 311) {
+            degree = 3
+        }
+        this.handleKneadDegree(degree)
+    },
 
-	// 控制温度
-	handleTemp: function (e) {
-		if (this.data.power) {
-			let temp = Number(this.data.temperature),
-				flag = Number(e.currentTarget.dataset.flag),
-				that = this;
-			if ((temp > 40 && flag != 1) || (temp < 60 && flag == 1)) {
-				wx.showLoading({
-					title: '控制中',
-				})
-				temp = flag == 1 ? (temp + 5) : (temp - 5)
-				let value = temp == 40 ? 40 : temp == 45 ? 50 : temp == 50 ? 60 : temp == 55 ? 80 : 100;
-				DeviceFunction.handleTemperature(AppData.connectingDeviceId, value).then(() => {
-					that.setData({
-						temperature: temp
-					}, () => {
-						wx.hideLoading()
-						wx.showToast({
-							title: '控制成功',
-							mask: false
-						})
-					})
-				})
-			}
-		} else {
-			wx.showToast({
-				title: '设备未开启',
-				icon: 'none'
-			})
-		}
-	},
+    // 控制温度
+    handleTemp: function (e) {
+        if (this.data.power) {
+            let temp = Number(this.data.temperature),
+                flag = Number(e.currentTarget.dataset.flag),
+                that = this;
+            if ((temp > 40 && flag != 1) || (temp < 60 && flag == 1)) {
+                wx.showLoading({
+                    title: '控制中',
+                })
+                temp = flag == 1 ? (temp + 5) : (temp - 5)
+                let value = temp == 40 ? 40 : temp == 45 ? 50 : temp == 50 ? 60 : temp == 55 ? 80 : 100;
+                DeviceFunction.handleTemperature(AppData.connectingDeviceId, value).then(() => {
+                    that.setData({
+                        temperature: temp
+                    }, () => {
+                        wx.hideLoading()
+                        wx.showToast({
+                            title: '控制成功',
+                            mask: false
+                        })
+                    })
+                })
+            }
+        } else {
+            wx.showToast({
+                title: '设备未开启',
+                icon: 'none'
+            })
+        }
+    },
 
-	// 揉捏方向
-	handleKneadModel: function () {
-		let value = this.data.kneadModel == 1 ? 2 : this.data.kneadModel == 2 ? 3 : this.data.kneadModel == 3 ? 0 : 1;
-		console.log()
-		if (value == 0) {
-			this.handleKnead(0, 0)
-		} else {
-			let degree = this.data.kneadDegree;
-			if (degree == 0) {
-				degree = 1
-			}
-			this.handleKnead(value, degree)
-		}
-	},
+    // 揉捏方向
+    handleKneadModel: function () {
+        let value = this.data.kneadModel == 1 ? 2 : this.data.kneadModel == 2 ? 3 : this.data.kneadModel == 3 ? 0 : 1;
+        console.log()
+        if (value == 0) {
+            this.handleKnead(0, 0)
+        } else {
+            let degree = this.data.kneadDegree;
+            if (degree == 0) {
+                degree = 1
+            }
+            this.handleKnead(value, degree)
+        }
+    },
 
-	// 揉捏力度
-	handleKneadDegree: function (degree) {
-		console.log(degree);
-		let value = this.data.kneadDegree;
-		if (this.data.kneadModel == 0) {
-			this.setData({
-				kneadDegree: 0
-			}, () => {
-				this.setData({
-					kneadDegree: value
-				})
-				wx.showToast({
-					title: '揉捏未开启',
-					icon: 'none'
-				})
-			})
-		} else if (degree == value) {
-			this.setData({
-				kneadDegree: 0
-			}, () => {
-				this.setData({
-					kneadDegree: value
-				})
-			})
-		} else {
-			this.handleKnead(this.data.kneadModel, degree)
-		}
-	},
+    // 揉捏力度
+    handleKneadDegree: function (degree) {
+        console.log(degree);
+        let value = this.data.kneadDegree;
+        if (this.data.kneadModel == 0) {
+            this.setData({
+                kneadDegree: 0
+            }, () => {
+                this.setData({
+                    kneadDegree: value
+                })
+                wx.showToast({
+                    title: '揉捏未开启',
+                    icon: 'none'
+                })
+            })
+        } else if (degree == value) {
+            this.setData({
+                kneadDegree: 0
+            }, () => {
+                this.setData({
+                    kneadDegree: value
+                })
+            })
+        } else {
+            this.handleKnead(this.data.kneadModel, degree)
+        }
+    },
 
-	// 揉捏
-	handleKnead: function (kneadModel, kneadDegree) {
-		if (this.data.power) {
-			let that = this;
-			wx.showLoading({
-				title: '控制中',
-			})
-			DeviceFunction.handleKnead(AppData.connectingDeviceId, kneadModel, kneadDegree).then(() => {
-				that.setData({
-					kneadModel: kneadModel,
-					kneadDegree: kneadDegree
-				}, () => {
-					wx.hideLoading();
-					wx.showToast({
-						title: '控制成功',
-						mask: false
-					})
-				})
-			})
-		} else {
-			this.setData({
-				kneadDegree: this.data.kneadDegree
-			}, () => {
-				wx.showToast({
-					title: '设备未开启',
-					icon: 'none'
-				})
-			})
-		}
-	},
+    // 揉捏
+    handleKnead: function (kneadModel, kneadDegree) {
+        if (this.data.power) {
+            let that = this;
+            wx.showLoading({
+                title: '控制中',
+            })
+            DeviceFunction.handleKnead(AppData.connectingDeviceId, kneadModel, kneadDegree).then(() => {
+                that.setData({
+                    kneadModel: kneadModel,
+                    kneadDegree: kneadDegree
+                }, () => {
+                    wx.hideLoading();
+                    wx.showToast({
+                        title: '控制成功',
+                        mask: false
+                    })
+                })
+            })
+        } else {
+            this.setData({
+                kneadDegree: this.data.kneadDegree
+            }, () => {
+                wx.showToast({
+                    title: '设备未开启',
+                    icon: 'none'
+                })
+            })
+        }
+    },
 
-	// 定时
-	handleSwitchTimer: function (timing) {
-		let value = this.data.timing;
-		if (this.data.power) {
-			if (timing == value) {
-				this.setData({
-					timing: 0
-				}, () => {
-					this.setData({
-						timing: value
-					})
-				})
-			} else {
-				let that = this;
-				wx.showLoading({
-					title: '控制中',
-				})
-				DeviceFunction.handleTimer(AppData.connectingDeviceId, timing).then(res => {
-					that.setData({
-						timing: timing,
-					}, () => {
-						wx.hideLoading()
-						wx.showToast({
-							title: '控制成功',
-							mask: false
-						})
-					})
-				})
-			}
-		} else {
-			this.setData({
-				timing: 0
-			}, () => {
-				this.setData({
-					timing: value
-				})
-				wx.showToast({
-					title: '设备未开启',
-					icon: 'none'
-				})
-			})
-		}
-	},
+    // 定时
+    handleSwitchTimer: function (timing) {
+        let value = this.data.timing;
+        if (this.data.power) {
+            if (timing == value) {
+                this.setData({
+                    timing: 0
+                }, () => {
+                    this.setData({
+                        timing: value
+                    })
+                })
+            } else {
+                let that = this;
+                wx.showLoading({
+                    title: '控制中',
+                })
+                DeviceFunction.handleTimer(AppData.connectingDeviceId, timing).then(res => {
+                    that.setData({
+                        timing: timing,
+                    }, () => {
+                        wx.hideLoading()
+                        wx.showToast({
+                            title: '控制成功',
+                            mask: false
+                        })
+                    })
+                })
+            }
+        } else {
+            this.setData({
+                timing: 0
+            }, () => {
+                this.setData({
+                    timing: value
+                })
+                wx.showToast({
+                    title: '设备未开启',
+                    icon: 'none'
+                })
+            })
+        }
+    },
 
-	// 开关机
-	handleSwichPower: function (e) {
-		wx.showLoading({
-			title: '控制中',
-		})
-		let that = this;
-		if (this.data.power) {
-			DeviceFunction.handleTimer(AppData.connectingDeviceId, 0).then(() => {
-				that.setData({
-					timing: 0,
-					power: false,
-					kneadModel: 0,
-					kneadDegree: 0,
-					temperature: 0,
-				}, () => {
-					wx.hideLoading()
-					wx.showToast({
-						title: '控制成功',
-						icon: 'none'
-					})
-				})
-			})
-		} else {
-			DeviceFunction.handleTimer(AppData.connectingDeviceId, 30).then(() => {
-				DeviceFunction.handleTemperature(AppData.connectingDeviceId, 60).then(() => {
-					DeviceFunction.handleKnead(AppData.connectingDeviceId, 3, 1).then(() => {
-						that.setData({
-							timing: 30,
-							power: true,
-							kneadModel: 3,
-							kneadDegree: 1,
-							temperature: 50,
-						}, () => {
-							wx.hideLoading()
-							wx.showToast({
-								title: '控制成功',
-								mask: false
-							})
-						})
-					})
-				})
-			})
-		}
-	},
+    // 开关机
+    handleSwichPower: function (e) {
+        wx.showLoading({
+            title: '控制中',
+        })
+        let that = this;
+        if (this.data.power) {
+            DeviceFunction.handleTimer(AppData.connectingDeviceId, 0).then(() => {
+                that.setData({
+                    timing: 0,
+                    power: false,
+                    kneadModel: 0,
+                    kneadDegree: 0,
+                    temperature: 0,
+                }, () => {
+                    wx.hideLoading()
+                    wx.showToast({
+                        title: '控制成功',
+                        icon: 'none'
+                    })
+                })
+            })
+        } else {
+            DeviceFunction.handleTimer(AppData.connectingDeviceId, 30).then(() => {
+                DeviceFunction.handleTemperature(AppData.connectingDeviceId, 60).then(() => {
+                    DeviceFunction.handleKnead(AppData.connectingDeviceId, 3, 1).then(() => {
+                        that.setData({
+                            timing: 30,
+                            power: true,
+                            kneadModel: 3,
+                            kneadDegree: 1,
+                            temperature: 50,
+                        }, () => {
+                            wx.hideLoading()
+                            wx.showToast({
+                                title: '控制成功',
+                                mask: false
+                            })
+                        })
+                    })
+                })
+            })
+        }
+    },
 
-	// 前往介绍
-	goInroduce: function () {
-		wx.navigateTo({
-			url: '../Introduce/Introduce',
-		})
-	}
+    // 前往介绍
+    goInroduce: function () {
+        wx.navigateTo({
+            url: '../Introduce/Introduce',
+        })
+    },
+
+    handleStartTemp: function (params) {
+        this.setData({
+            tempPower: !this.data.tempPower,
+            temperature: this.data.tempPower ? 0 : 50,
+            tempTiminig: this.data.tempPower ? 0 : 30,
+        })
+    },
+    handleChoseTempTiming: function () {
+        this.setData({
+            showDialog: true
+        })
+    },
+    handleConfirm: function (val) {
+        this.setData({
+            tempPower: true,
+            temperature: this.data.tempPower ? this.data.temperature : 50,
+            tempTiminig: val.detail.value,
+            showDialog: false
+        })
+    }
 })
--- utils/BLE/deviceFuntion.js
+++ utils/BLE/deviceFuntion.js
@@ -4,133 +4,134 @@ import BleTools from './bleTools';
 
 // 温控
 // 初始化长度 1 个字节，可读可写，当前可写最大值 100
-const handleTemperature = function (deviceId, temp) {
-	return new Promise(function (resolve, reject) {
-		console.log('写入温度', temp);
-		let value = Utils.hex2ab(Utils.int2hex(temp));
-		BleTools.handleWrite(deviceId, AppData.services.temp[0], AppData.services.temp[1], value).then(() => {
-			resolve()
-		}).catch(err => {
-			wx.hideLoading()
-			wx.showToast({
-				title: '设置失败',
-				icon: 'none',
-			})
-		})
-	})
+const handleTemperature = function (deviceId, temp, time) {
+    return new Promise(function (resolve, reject) {
+        console.log('写入温度', temp);
+        let value = Utils.hex2ab(Utils.int2hex(temp));
+        BleTools.handleWrite(deviceId, AppData.services.temp[0], AppData.services.temp[1], value).then(() => {
+            resolve()
+        }).catch(err => {
+            wx.hideLoading();
+            wx.showToast({
+                title: '设置失败',
+                icon: 'none',
+            })
+            reject(err)
+        })
+    })
 }
 
 // 定时
 // 初始化长度 2 个字节，可读可写，定时范围 10-720
 const handleTimer = function (deviceId, time) {
-	return new Promise(function (resolve, reject) {
-		time = Number(time)
-		console.log(time);
-		let num = time.toString(16);
-		if (num.length == 1) {
-			num = '000' + num
-		} else if (num.length == 2) {
-			num = '00' + num
-		} else if (num.length == 3) {
-			num = '0' + num
-		}
-		console.log('写入时间', time, '写入数值', num);
-		let value = Utils.hex2ab(num);
-		BleTools.handleWrite(deviceId, AppData.services.timing[0], AppData.services.timing[1], value).then(() => {
-			resolve()
-		}).catch(err => {
-			wx.hideLoading()
-			wx.showToast({
-				title: '设置失败',
-				icon: 'none',
-			})
-		})
-	})
+    return new Promise(function (resolve, reject) {
+        time = Number(time)
+        console.log(time);
+        let num = time.toString(16);
+        if (num.length == 1) {
+            num = '000' + num
+        } else if (num.length == 2) {
+            num = '00' + num
+        } else if (num.length == 3) {
+            num = '0' + num
+        }
+        console.log('写入时间', time, '写入数值', num);
+        let value = Utils.hex2ab(num);
+        BleTools.handleWrite(deviceId, AppData.services.timing[0], AppData.services.timing[1], value).then(() => {
+            resolve()
+        }).catch(err => {
+            wx.hideLoading()
+            wx.showToast({
+                title: '设置失败',
+                icon: 'none',
+            })
+        })
+    })
 }
 
 // 按摩
 // 第 1 个字节:按摩模式，'01/'02/'03/'04/'05 代表模式 1-4 及默认模式 
 // 第 2 个字节:按摩力度，'01/'02/'03 代表按摩力度高、中、低
-const handleMassage = function (deviceId, model, degree) {
-	let fir = '0' + model,
-		sec = '0' + degree;
-	console.log('写入按摩', fir, sec);
-	return new Promise(function (resolve, reject) {
-		let value = Utils.hex2ab(fir + sec)
-		BleTools.handleWrite(deviceId, AppData.services.massage[0], AppData.services.massage[1], value).then(() => {
-			resolve()
-		}).catch(err => {
-			wx.hideLoading()
-			wx.showToast({
-				title: '设置失败',
-				icon: 'none',
-			})
-		})
-	})
+const handleMassage = function (deviceId, model, degree, time) {
+    let fir = '0' + model,
+        sec = '0' + degree;
+    console.log('写入按摩', fir, sec);
+    return new Promise(function (resolve, reject) {
+        let value = Utils.hex2ab(fir + sec)
+        BleTools.handleWrite(deviceId, AppData.services.massage[0], AppData.services.massage[1], value).then(() => {
+            resolve()
+        }).catch(err => {
+            wx.hideLoading()
+            wx.showToast({
+                title: '设置失败',
+                icon: 'none',
+            })
+        })
+    })
 }
 
 // 通风
 // 初始化长度 1 个字节，可读可写，'01/'02/'03 代表通风高、中、低
-const handleVentilation = function (deviceId, degree) {
-	degree = '0' + degree;
-	console.log('写入通风', degree)
-	return new Promise(function (resolve, reject) {
-		let value = Utils.hex2ab(degree)
-		BleTools.handleWrite(deviceId, AppData.services.ventilation[0], AppData.services.ventilation[1], value).then(() => {
-			resolve()
-		}).catch(err => {
-			wx.hideLoading()
-			wx.showToast({
-				title: '设置失败',
-				icon: 'none',
-			})
-		})
-	})
+const handleVentilation = function (deviceId, degree, time) {
+    degree = '0' + degree;
+    console.log('写入通风', degree)
+    return new Promise(function (resolve, reject) {
+        let value = Utils.hex2ab(degree)
+        BleTools.handleWrite(deviceId, AppData.services.ventilation[0], AppData.services.ventilation[1], value).then(() => {
+            resolve()
+        }).catch(err => {
+            wx.hideLoading()
+            wx.showToast({
+                title: '设置失败',
+                icon: 'none',
+            })
+        })
+    })
 }
 
 // 揉捏
 // 第 1 个字节:揉捏方向，'01/'02/'03 代表正、反转、自动切换 
 // 第 2 个字节:揉捏力度，'01/'02/'03 代表高、中、低
-const handleKnead = function (deviceId, direction, degree) {
-	direction = '0' + direction
-	degree = '0' + degree
-	console.log('写入揉捏', direction, degree)
-	return new Promise(function (resolve, reject) {
-		let value = Utils.hex2ab(direction + degree)
-		BleTools.handleWrite(deviceId, AppData.services.knead[0], AppData.services.knead[1], value).then(() => {
-			resolve();
-		}).catch(err => {
-			console.log(err)
-			wx.hideLoading();
-			wx.showToast({
-				title: '设置失败',
-				icon: 'none',
-			})
-		})
-	})
+const handleKnead = function (deviceId, direction, degree, time) {
+    direction = '0' + direction
+    degree = '0' + degree
+    console.log('写入揉捏', direction, degree)
+    return new Promise(function (resolve, reject) {
+        let value = Utils.hex2ab(direction + degree)
+        BleTools.handleWrite(deviceId, AppData.services.knead[0], AppData.services.knead[1], value).then(() => {
+            resolve();
+        }).catch(err => {
+            console.log(err)
+            wx.hideLoading();
+            wx.showToast({
+                title: '设置失败',
+                icon: 'none',
+            })
+        })
+    })
 }
 
 // 充放气
 // 初始化长度 1 个字节，可读可写，'01/'02/'03 代表充气、放气、自动充放气
-const handleGas = function (deviceId, model) {
-	model = '0' + model;
-	console.log('写入充放气', model)
-	console.log('服务', AppData.services.gas[0], AppData.services.gas[1])
-	return new Promise(function (resolve, reject) {
-		let value = Utils.hex2ab(model);
-		BleTools.handleWrite(deviceId, AppData.services.gas[0], AppData.services.gas[1], value).then(() => {
-			resolve()
-		}).catch(err => {
-			reject(err)
-		})
-	})
+const handleGas = function (deviceId, model, time) {
+    model = '0' + model;
+    console.log('写入充放气', model)
+    console.log('服务', AppData.services.gas[0], AppData.services.gas[1])
+    return new Promise(function (resolve, reject) {
+        let value = Utils.hex2ab(model);
+        BleTools.handleWrite(deviceId, AppData.services.gas[0], AppData.services.gas[1], value).then(() => {
+            resolve()
+        }).catch(err => {
+            reject(err)
+        })
+    })
 }
 
 module.exports = {
-	handleTemperature: handleTemperature,
-	handleTimer: handleTimer,
-	handleMassage: handleMassage,
-	handleVentilation: handleVentilation,
-	handleKnead: handleKnead,
-	handleGas: handleGas
+    handleTemperature: handleTemperature,
+    handleTimer: handleTimer,
+    handleMassage: handleMassage,
+    handleVentilation: handleVentilation,
+    handleKnead: handleKnead,
+    handleGas: handleGas
 }
